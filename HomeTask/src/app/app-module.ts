20a5bc1adaf1de4dca8e0b74ba32e2e6cd79f3dc
Suprova Shemanto <suprovashemanto@gmail.com>
2025-06-22 00:17:13 +0600

    add

---
+++ HomeTask/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { HomeComponent } from './home-component/home-component';
+
+@NgModule({
+  declarations: [
+    App,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
